c3143b669c5182ebddc0df285934ea25879a3bcb
Simon-Njoroge <mukirisimon22@gmail.com>
2024-05-17 11:25:09 +0300

    todo project

---
+++ todo-app-main/index.js
@@ -0,0 +1,12 @@
+let x = document.getElementsByClassName("check1");
+x.addEventListener("onselect", ()=>{
+    let y=docume.getElementsByClassName("p1").value
+y.style.color="red";
+document.getElementsByClassName("p1").innerhtml=y;
+});
+
+let par1= document.getElementsByClassName("inp1");
+apr1.addEventListener("onmouseover",()=>{
+    par1 += "x";
+    document.getElementsByClassName("inp1").innerhtml=par1;
+})
